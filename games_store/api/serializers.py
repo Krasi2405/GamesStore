7d63ec3da2314f583dcb5fc16c6aea5b8bfc2acb
Krasimir Georgiev <krasi2405@gmail.com>
2019-06-05 18:08:59 +0300

    api put and get game thumbnail using base64

--- games_store/api/serializers.py
+++ games_store/api/serializers.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from django.conf import settings
+from django.core.files.uploadedfile import UploadedFile
+from django.core.files.base import ContentFile
+
 
 from django.core.files import File
 import base64
@@ -14,26 +18,46 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ('id', 'username', 'email', 'game_set')
 
+class ImageSerializerField(serializers.Field):
+
+	def get_attribute(self, instance):
+		# We pass the object instance onto `to_representation`,
+		# not just the field attribute.
+		return instance.thumbnail
+
+	def to_representation(self, value):
+		if not value:
+			return None
+
+		f = open(value.path, 'rb')
+		image = File(f)
+		data = base64.b64encode(image.read())
+		f.close()
+
+		return data
+
+	def to_internal_value(self, data):
+		if isinstance(data, UploadedFile):
+			return data
+		else:
+			format, imgstr = data.split(';base64,') 
+			ext = format.split('/')[-1] 
+
+			data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+			return data
+
 
 class GameSerializer(serializers.HyperlinkedModelSerializer):
 	tags = serializers.PrimaryKeyRelatedField(many=True, queryset=Tag.objects.all())
 	platforms = serializers.PrimaryKeyRelatedField(many=True, queryset=Platform.objects.all())
 
-	thumbnail_download = serializers.SerializerMethodField(allow_null=True, read_only=True)
+	thumbnail = ImageSerializerField(allow_null=True)
 
-	def get_thumbnail(self, obj):
-		if not obj.thumbnail:
-			return None
-
-		f = open(obj.thumbnail.path, 'rb')
-		image = File(f)
-		data = base64.b64encode(image.read())
-		f.close()
-		return data
 
 	class Meta:
 		model = Game
-		fields = ('id', 'name', 'thumbnail', 'thumbnail_download', 'description', 'price', 'release_date', 'tags', 'platforms')
+		fields = ('id', 'name', 'thumbnail', 'description', 'price', 'release_date', 'tags', 'platforms')
+
 
 
 class TagSerializer(serializers.HyperlinkedModelSerializer):
--- games_store/games_store/settings.py
+++ games_store/games_store/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'store',
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'games_store.urls'
@@ -152,3 +154,5 @@ REST_FRAMEWORK = {
     #    'rest_framework.permissions.IsAuthenticated',
     #),
 }
+
+CORS_ORIGIN_ALLOW_ALL =  True
